501e893cb16805cfb04519c60cff69c92379be79
Keerthi-U <keerthiiachu173@gmail.com>
2025-02-19 14:41:57 +0000

    all file added

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const [list] = useState([
+    {
+      name: "Keerthi",
+      image: "https://via.placeholder.com/150",
+      dob: "1996-12-21", // Ensure the date format is YYYY-MM-DD
+    },
+    {
+      name: "Achu",
+      image: "https://via.placeholder.com/150",
+      dob: "1997-04-08", // Ensure the date format is YYYY-MM-DD
+    },
+    {
+      name: "Zakir",
+      image: "https://via.placeholder.com/150",
+      dob: "1996-01-01", // Ensure the date format is YYYY-MM-DD
+    },
+    {
+      name: "Zakirkeerthi",
+      image: "https://via.placeholder.com/150",
+      dob: "1996-01-15", // Ensure the date format is YYYY-MM-DD
+    },
+  ]);
+
+  const [setUpcoming, setUpcomingBirthdays] = useState(list);
+
+  function showUpcomingBirthdays() {
+    // console.log("list1" , list3);
+
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    // console.log(today);
+    // console.log(currentYear);
+
+    const filteredData = list.filter((person) => {
+      const person_dob = new Date(person.dob);
+
+      const birthyear = new Date(
+        currentYear,
+        person_dob.getMonth(),
+        person_dob.getDate()
+      );
+
+      // console.log(birthyear);
+      // console.log(today);
+      return birthyear >= today;
+    });
+    setUpcomingBirthdays(filteredData);
+  }
+
+  console.log(setUpcoming);
+
+  function calculateAge(dob) {
+    const Today = new Date();
+    const birthdayDate = new Date(dob);
+    // alert(birthdayDate)
+    const getDay = Today.getDate() - birthdayDate.getDate();
+    let age = Today.getFullYear() - birthdayDate.getFullYear();
+    const getMonth = Today.getMonth() - birthdayDate.getMonth();
+
+    if (getMonth < 0 || (getMonth === 0 && getDay < 0)) {
+      age--;
+    }
+    return age;
+  }
+
+  console.log(list);
+
+  return (
+    <div>
+      <main>
+        <section className="container">
+          <article>
+            <div className="upcomeing">
+              <h3>birthdays today</h3>
+              <button onClick={showUpcomingBirthdays}>
+                Show Upcoming Birthdays
+              </button>
+              <button onClick={() => setUpcomingBirthdays(list)}>
+                Show All Birthdays
+              </button>
+            </div>
+          </article>
+          <section>
+            {setUpcoming.map((item, index) => (
+              // console.log(item.name);
+
+              <article className="person" key={index}>
+                <img src="https://via.placeholder.com/150" alt="" />
+
+                <div>
+                  <h4>{item.name}</h4>
+                  <p>{calculateAge(item.dob)}Years old</p>
+                </div>
+              </article>
+            ))}
+          </section>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
